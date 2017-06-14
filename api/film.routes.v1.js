8a8d110b0e2c4d4c6da87a9cf883f8f12cfb426c
Devin <dajbraat@avans.nl>
2017-06-14 17:25:11 +0200

    Endpoint 6 Met error

--- api/film.routes.v1.js
+++ api/film.routes.v1.js
@@ -2,15 +2,22 @@ var express = require('express');
 var routes = express.Router();
 var db = require('../config/db');
 
-// Film ophalen aan de hand van het film id
+// Maakt een nieuwe uitlening voor de gegeven gebruiker van het exemplaar met gegeven inventoryid
+routes.post('/rentals/:customer_id/:inventory_id', function(req, res) {
 
-routes.get('/rentals/:rental_id', function(req, res) {
+    var customerId = req.params.customer_id;
+    var inventoryId = req.params.inventory_id;
+    var query = {
+        sql: 'INSERT INTO `rental`(`customer_id`, `inventory_id`) VALUES (?, ?)',
+        values: [customerId, inventoryId],
+        timeout: 2000 // 2secs
+    };
 
-    var rentalId = req.params.rental_id;
+    // console.dir(rentals);
+    // console.log('Onze query: ' + query.sql);
 
     res.contentType('application/json');
-
-    db.query('SELECT * FROM rental WHERE RENTAL_ID=?', [rentalId], function(error, rows, fields) {
+    db.query(query, function(error, rows, fields) {
         if (error) {
             res.status(401).json(error);
         } else {
@@ -20,6 +27,24 @@ routes.get('/rentals/:rental_id', function(req, res) {
 });
 
 
+
+// Alle uitgeleende films aan de hand van rental_id
+routes.get('/rentals/:rental_id', function(req, res) {
+
+    var rentalId = req.params.rental_id;
+
+    res.contentType('application/json');
+
+    db.query('SELECT * FROM rental WHERE rental_id=?', [rentalId], function(error, rows, fields) {
+        if (error) {
+            res.status(401).json(error);
+        } else {
+            res.status(200).json({ result: rows });
+        };
+    });
+});
+
+// Film ophalen aan de hand van het film id
 routes.get('/films/:film_id', function(req, res) {
 
     var filmsFilmid = req.params.film_id;
