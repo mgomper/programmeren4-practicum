0b7f77b44c58b626c85e3697468598bb3c0b9898
SemBuermans <S.Buermans@homtail.com>
2017-06-14 16:07:09 +0200

    endpoint 5 toegevoegd

--- api/film.routes.v1.js
+++ api/film.routes.v1.js
@@ -4,6 +4,22 @@ var db = require('../config/db');
 
 // Film ophalen aan de hand van het film id
 
+routes.get('/rentals/:rental_id', function(req, res) {
+
+    var rentalId = req.params.rental_id;
+
+    res.contentType('application/json');
+
+    db.query('SELECT * FROM rental WHERE RENTAL_ID=?', [rentalId], function(error, rows, fields) {
+        if (error) {
+            res.status(401).json(error);
+        } else {
+            res.status(200).json({ result: rows });
+        };
+    });
+});
+
+
 routes.get('/films/:film_id', function(req, res) {
 
     var filmsFilmid = req.params.film_id;
@@ -18,6 +34,7 @@ routes.get('/films/:film_id', function(req, res) {
         };
     });
 });
+
 
 module.exports = routes;
 
