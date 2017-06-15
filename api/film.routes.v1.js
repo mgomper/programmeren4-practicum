0079bea4ad6afcb1af8253cee72155efb96b15c4
Devin <dajbraat@avans.nl>
2017-06-15 11:45:18 +0200

    Alle endpoints af

--- api/film.routes.v1.js
+++ api/film.routes.v1.js
@@ -3,9 +3,41 @@ var routes = express.Router();
 var db = require('../config/db');
 
 
+// Endpoint 4 - Film informatie ophalen aan de hand van film_id
+routes.get('/films/:film_id', function(req, res) {
+
+    var filmsFilmid = req.params.film_id;
+
+    res.contentType('application/json');
+
+    db.query('SELECT * FROM film WHERE FILM_ID=?', [filmsFilmid], function(error, rows, fields) {
+        if (error) {
+            res.status(401).json(error);
+        } else {
+            res.status(200).json({ result: rows });
+        };
+    });
+});
 
 
-///Maakt een nieuwe uitlening voor de gegeven gebruiker van het exemplaar met gegeven inventoryid.
+// Endpoint 5 - Alle uitgeleende films aan de hand van rental_id
+routes.get('/rentals/:rental_id', function(req, res) {
+
+    var rentalId = req.params.rental_id;
+
+    res.contentType('application/json');
+
+    db.query('SELECT * FROM rental WHERE rental_id=?', [rentalId], function(error, rows, fields) {
+        if (error) {
+            res.status(401).json(error);
+        } else {
+            res.status(200).json({ result: rows });
+        };
+    });
+});
+
+
+// Endpoint 6 - Maakt een nieuwe uitlening voor de gegeven gebruiker van het exemplaar met gegeven inventory_id.
 routes.post('/rentals/:userid/:inventoryid', function(req, res) {
 
     var user = req.params.userid;
@@ -23,25 +55,7 @@ routes.post('/rentals/:userid/:inventoryid', function(req, res) {
 });
 
 
-//uitlening verwijderen
-routes.delete('/rentals/:userid/:inventoryid', function(req, res) {
-
-    var user = req.params.userid;
-    var inventory = req.params.inventoryid;
-
-
-    res.contentType('application/json');
-
-    db.query('DELETE FROM rental WHERE customer_id=? AND inventory_id=?', [user, inventory], function(error, rows, fields) {
-        if (error) {
-            res.status(401).json(error);
-        } else {
-            res.status(200).json({ result: rows });
-        };
-    });
-});
-
-//uitlening wijzigen
+// Endpoint 7 - Bestaande uitlening wijzigen
 routes.put('/rentals/:userid/:inventoryid', function(req, res) {
 
     // var user = req.params.userid;
@@ -60,31 +74,16 @@ routes.put('/rentals/:userid/:inventoryid', function(req, res) {
 });
 
 
+//Endpoint 8 - Bestaande uitlening verwijderen
+routes.delete('/rentals/:userid/:inventoryid', function(req, res) {
 
-// Alle uitgeleende films aan de hand van rental_id
-routes.get('/rentals/:rental_id', function(req, res) {
+    var user = req.params.userid;
+    var inventory = req.params.inventoryid;
 
-    var rentalId = req.params.rental_id;
-
-    res.contentType('application/json');
-
-    db.query('SELECT * FROM rental WHERE rental_id=?', [rentalId], function(error, rows, fields) {
-        if (error) {
-            res.status(401).json(error);
-        } else {
-            res.status(200).json({ result: rows });
-        };
-    });
-});
-
-// Film ophalen aan de hand van het film id
-routes.get('/films/:film_id', function(req, res) {
-
-    var filmsFilmid = req.params.film_id;
 
     res.contentType('application/json');
 
-    db.query('SELECT * FROM film WHERE FILM_ID=?', [filmsFilmid], function(error, rows, fields) {
+    db.query('DELETE FROM rental WHERE customer_id=? AND inventory_id=?', [user, inventory], function(error, rows, fields) {
         if (error) {
             res.status(401).json(error);
         } else {
