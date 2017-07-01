f3fd583344be4b0c75b976fd4c090e212beb3117
mgomper <mgomper@avans.nl>
2017-07-02 00:49:40 +0200

    test

--- test/authentication.routes.v1.test.js
+++ test/authentication.routes.v1.test.js
@@ -14,12 +14,14 @@ describe('Auth API v1', function() {
 //kijkt simpelweg of er een object binnenkomt
     it('returns UnauthorizedError on GET /api/v1/todos when not logged in', function(done) {
         chai.request(require('../server.js'))
-            .get('/api/v1/films')
+            .get('/api/v1/films/1')
+            .set('Authorization', "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0OTkxMjE5OTIsImlhdCI6MTQ5ODk0OTE5Miwic3ViIjoiNDYifQ.873Ob-Meza7nqlO0DvY7kBwkS6v8ezyzSZHfAjLCNSo")
             .end(function(err, res) {
                 res.should.have.status(200);
                 res.should.be.json;
                 res.body.should.be.a('object');
-                res.body.should.have.property('error');
+                res.body.should.have.property('result').that.is.an('array');
+                res.body.result.should.have.lengthOf(13);
                 done();
             });
     });
