3c8f9655e53fcf0624ef755d6b5db1ec4013131b
mgomper <mgomper@avans.nl>
2017-07-02 18:40:27 +0200

    final tests

--- test/routes.v1.test.js
+++ test/routes.v1.test.js
@@ -29,12 +29,11 @@ var getToken = function() {
             res.body.should.be.an('object');
             res.body.should.have.property('token');
             res.should.have.status(200);
-            // Bewaar het token in de globale variabele, zodat de tests het token kunnen gebruiken.
             value = res.body.token;
         });
 }
 
-it('should register test account on POST at /api/v1/register', function(done){
+it('should not register test account on POST at /api/v1/register when account already exists', function(done){
         chai.request(server)
             .post('/api/v1/register')
             .send({"username":"55", "password":"test"})
@@ -50,9 +49,6 @@ it('should register test account on POST at /api/v1/register', function(done){
 
 describe('Get a token', function() {
 
-    // Zorg dat we een token hebben zodat we de tests kunnen uitvoeren.
-
-    // Hier start een testcase
     it('should return a valid token', function(done) {
         chai.request(server)
             .post('/api/v1/login')
@@ -66,80 +62,66 @@ describe('Get a token', function() {
     });
 });
 
-describe('Get a valid token', function() {
-    // Hier start een testcase
-    it('should return a valid token', function(done) {
+describe('Get a specific rental', function() {
+    it('should return a specific rental', function(done) {
         chai.request(server)
             .get('/api/v1/rentals/1')
             .set('Content-Type', 'application/json')
             .set('Authorization', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0OTkxNzIzMzksImlhdCI6MTQ5ODk5OTUzOSwic3ViIjoiNDYifQ.OF01Vu_gMIw_RUpv9Hnjo0Win0RDIjOcQv6tqWwuc5M')
             .end(function(err, res) {
               res.body.should.have.property('result').that.is.an('array');
+              res.body.should.have.property('film_id');
+              res.body.should.have.property('title');
+              res.body.should.have.property('description');
+              res.body.should.have.property('active');
+              res.body.should.have.property('customer_id');
               res.should.have.status(200);
-                // we doen hier niets - we willen alleen het token dat opgehaald is.
                 done();
             });
     });
 });
 
-describe('Get a valid token', function() {
-    // Hier start een testcase
-    it('should return a valid token', function(done) {
+describe('Get a specific film', function() {
+    it('should return a specific film', function(done) {
         chai.request(server)
             .get('/api/v1/films/1')
             .set('Content-Type', 'application/json')
             .set('Authorization', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0OTkxNzIzMzksImlhdCI6MTQ5ODk5OTUzOSwic3ViIjoiNDYifQ.OF01Vu_gMIw_RUpv9Hnjo0Win0RDIjOcQv6tqWwuc5M')
             .end(function(err, res) {
               res.body.should.have.property('result').that.is.an('array');
+              res.body.should.have.property('film_id');
+              res.body.should.have.property('title');
               res.should.have.status(200);
-                // we doen hier niets - we willen alleen het token dat opgehaald is.
                 done();
             });
     });
 });
 
-describe('Get a valid token', function() {
-    // Hier start een testcase
-    it('should return a valid token', function(done) {
-        chai.request(server)
-            .get('/api/v1/films/1')
-            .set('Content-Type', 'application/json')
-            .set('Authorization', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0OTkxNzIzMzksImlhdCI6MTQ5ODk5OTUzOSwic3ViIjoiNDYifQ.OF01Vu_gMIw_RUpv9Hnjo0Win0RDIjOcQv6tqWwuc5M')
-            .end(function(err, res) {
-            res.body.should.have.property('result').that.is.an('array');
-            res.should.have.status(200);
-                done();
-            });
-    });
-});
-
-describe('Get a valid token', function() {
-    // Hier start een testcase
-    it('should return a valid token', function(done) {
+describe('Get a no auth message', function() {
+    it('should return an error', function(done) {
         chai.request(server)
             .get('/api/v1/films/1')
             .set('Content-Type', 'application/json')
             .set('Authorization', 'geen valide token')
             .end(function(err, res) {
               res.body.should.not.have.property('result').that.is.an('array');
-                // we doen hier niets - we willen alleen het token dat opgehaald is.
+              res.body.should.have.property('error');
                 done();
             });
     });
 });
 
 
-describe('Auth API v1', function() {
-//kijkt simpelweg of er een object binnenkomt
-    it('returns UnauthorizedError on GET /api/v1/todos when not logged in', function(done) {
-        chai.request(require('../server.js'))
-            .get('/api/v1/films/1')
+describe('Get a rental count', function() {
+    it('should return a rental count', function(done) {
+        chai.request(server)
+            .get('/api/v1/rentalcount/1')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0OTkxNzIzMzksImlhdCI6MTQ5ODk5OTUzOSwic3ViIjoiNDYifQ.OF01Vu_gMIw_RUpv9Hnjo0Win0RDIjOcQv6tqWwuc5M')
             .end(function(err, res) {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('object');
+              res.body.should.have.property('COUNT(inventory_id)');
+              res.should.have.status(200);
                 done();
             });
     });
-
 });
