424f7550df47a08773727c471c9c33f0a909d684
mgomper <mgomper@avans.nl>
2017-07-02 18:48:55 +0200

    test

--- test/routes.v1.test.js
+++ test/routes.v1.test.js
@@ -84,8 +84,8 @@ describe('Get a specific film', function() {
             .set('Authorization', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0OTkxNzIzMzksImlhdCI6MTQ5ODk5OTUzOSwic3ViIjoiNDYifQ.OF01Vu_gMIw_RUpv9Hnjo0Win0RDIjOcQv6tqWwuc5M')
             .end(function(err, res) {
               res.body.should.have.property('result').that.is.an('array');
-              res.body.should.have.property('film_id');
-              res.body.should.have.property('title');
+              res.body.result.should.have.property('film_id');
+              res.body.result.should.have.property('title');
               res.should.have.status(200);
                 done();
             });
